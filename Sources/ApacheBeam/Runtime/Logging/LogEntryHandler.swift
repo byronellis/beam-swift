cfdb43d443c5ec96a56f4545c46c79e155927c49
Byron Ellis <byronellis@google.com>
2023-11-03 05:51:10 -0700

    Update GRPC to latest version (and make the expansion service integration test behave better) Adding Metrics package and starting to add metrics handling Stream-based log handler to capture startup logs before we can connect to a log destination

---
+++ Sources/ApacheBeam/Runtime/Logging/LogEntryHandler.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Byron Ellis on 10/19/23.
+//
+
+import Foundation
+import Logging
+
+typealias LogStream = AsyncStream<Org_Apache_Beam_Model_FnExecution_V1_LogEntry>
+
+public struct LogEntryHandler : LogHandler {
+    
+}
---
+++ Sources/ApacheBeam/Runtime/Metrics/MetricValue.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Byron Ellis on 11/2/23.
+//
+
+import Foundation
